7c51c07bc2ec3e5cc10d6c01841713c38c541a6d
kapi2700 <kapi2700@tlen.pl>
2022-12-13 01:13:00 +0100

    dijstra, and exporting to file

--- nodes.hh
+++ nodes.hh
@@ -1,16 +1,36 @@
 #pragma once
+
 #include "world.hh"
 #include <stdlib.h>
 #include <time.h>
 #include <cmath>
 #include <iostream>
+#include <bits/stdc++.h>
+//#include "dijstra.hh"
+
+//static int table[100][100];
+
+
+
+
+#define INF 0x3f3f3f3f
+// iPair ==> Integer Pair
+typedef pair<int, int> iPair;
+
+void addEdge(vector<pair<int, int>> adj[], int u, int v, int wt);
+
+vector<int> shortestPath(vector<pair<int, int>> adj[], int V, int src, int target);
+
 
 struct node
 {
     float pos[2];
-    std::vector<node*> connectedNodes;
+    std::vector<pair<int,int>> connectedNodes;
+    // std::vector<node*> connectedNodes;
+    // std::vector<float> distanceToNode;
     int connections=0;
 public:
+    int nodeID=0;
     bool wasChecked=false;
     bool isTheRoute=false;
     node()
@@ -40,9 +60,11 @@ class nodes
 {
     std::vector<node> N;
 public:
+    std::vector <pair<pair<int,int>,int>> allEdges;
     world *W;
     int connections=0;
-
+    void addStart(float x, float y, int R);
+    void addEnd(float x, float y, int R);
     void generateNodes(int amount, float R); // amount ilość nodeów do generacji, R- odległość łącząca node'y
 
     bool nodeColissionCheck(node checkedNode);
@@ -62,7 +84,7 @@ public:
     {
         ofstream save;
         save.open("out.txt");
-        for(int i=0; i<N.size();i++)
+        for(long unsigned int i=0; i<N.size();i++)
         {
             save<<N[i].pos[0]<<"   "<<N[i].pos[1]<<endl;
             save<<endl<<endl;
@@ -88,4 +110,5 @@ public:
             // }
         }
     }
+    void findDistance();
 };
